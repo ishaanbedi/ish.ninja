926978fb263cc94136df70781b1c8776965b6239
Ishaan Bedi <bediishaan6@gmail.com>
2023-05-07 15:39:04 +0530

    Add umami tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <script async src="https://analytics.umami.is/script.js" data-website-id="44abb7de-69ac-4501-a213-6bca4a16fe13"></script>
       </body>
     </Html>
   )
